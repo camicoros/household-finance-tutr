bd56b307eb165fee0219cc33af209fdddab0b33b
Denis <d.saushkin@directline.digital>
2022-06-01 10:36:33 +0400

    add child window and btn

--- main.py
+++ main.py
@@ -4,6 +4,42 @@ import tkinter as tk
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        toolbar = tk.Frame(bg='#d7d8e0', bd=2)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        self.add_img = tk.PhotoImage(file='add.gif')
+
+        btn_open_dialog = tk.Button(
+            toolbar,
+            text='Добавить позицию',
+            command=self.open_dialog,
+            bg='#d7d8e0',
+            bd=0,
+            compound=tk.TOP,
+            image=self.add_img,
+        )
+        btn_open_dialog.pack(side=tk.LEFT)
+
+    def open_dialog(self):
+        Child()
+
+
+class Child(tk.Toplevel):
+    def __init__(self):
+        super().__init__(root)
+        self.init_child()
+
+    def init_child(self):
+        self.title('Добавить доходы/расходы')
+        self.geometry('400x220+400+300')
+        self.resizable(False, False)
+        # перехват всех событий
+        self.grab_set()
+        # захват фокуса
+        self.focus_get()
 
 
 if __name__ == '__main__':
